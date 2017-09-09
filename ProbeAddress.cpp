0c1c7627a4dc15d2f81d478ac8ffb85cf0735e87
Gong Cun <gong_cun@bocmacau.com>
2017-09-09 20:00:31 +0800

    upd

--- ProbeAddress.cpp
+++ ProbeAddress.cpp
@@ -1,9 +1,10 @@
 #include "ProbeRoute.hpp"
-// using namespace std;
 
-ProbeAddress::ProbeAddress(const char *host, const char *service) throw(ProbeException)
+ProbeAddress::ProbeAddress(const char *foreignHost, const char *foreignService,
+                           const char *localHost, int localPort) throw(ProbeException)
 {
-    struct addrinfo hints, *res;
+    struct addrinfo hints, *res, *curr;
+    struct sockaddr_in *paddr;
     int n;
 
     bzero(&hints, sizeof(struct addrinfo));
@@ -12,36 +13,42 @@ ProbeAddress::ProbeAddress(const char *host, const char *service) throw(ProbeExc
     hints.ai_family = AF_INET;
     hints.ai_socktype = 0;
 
-    if ((n = getaddrinfo(host, service, &hints, &res)) != 0)
+    if ((n = getaddrinfo(foreignHost, foreignService, &hints, &res)) != 0)
         throw ProbeException("getaddrinfo error", gai_strerror(n));
 
-    bzero(&addr, sizeof(struct sockaddr));
-    memcpy(&addr, res->ai_addr, sizeof(struct sockaddr));
-    addrlen = res->ai_addrlen;
+    bzero(&localAddr, sizeof(struct sockaddr));
 
+    sockfd = -1;
+    for (curr = res; !curr && sockfd < 0; curr = curr->ai_next)
+	if ((sockfd = socket(curr->ai_family, SOCK_DGRAM, 0)) >= 0) {
+	    if (connect(sockfd, curr->ai_addr, curr->ai_addrlen) < 0) {
+		close(sockfd);
+		sockfd = -1;
+	    }
+	}
+    
+    if (sockfd < 0) {
+	freeaddrinfo(res);
+	throw ProbeException("Unable to connect by UDP");
+    }
+	
+    localAddrLen = curr->ai_addrlen;
+    memcpy(&localAddr, curr->ai_addr, localAddrLen);
     freeaddrinfo(res);
-}
 
-ProbeAddress::ProbeAddress(const char *host, int port) throw(ProbeException)
-{
-    char service[6];
-    struct addrinfo hints, *res;
-    int n;
+    paddr = (struct sockaddr_in *)&localAddr;
 
-    snprintf(service, sizeof(service), "%d", port);
-    bzero(&hints, sizeof(struct addrinfo));
+    if (localHost == NULL) {
+        if (getsockname(sockfd, &localAddr, &localAddrLen) < 0)
+            throw ProbeException("getsockname");
+    } else {
+        if (inet_pton(AF_INET, localHost, &paddr->sin_addr) != 1)
+            throw ProbeException("inet_pton error");
+        localAddrLen = sizeof(struct sockaddr_in);
+    }
 
-    hints.ai_flags = AI_CANONNAME;
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = 0;
+    // anyway, we define the local port ourselves
+    paddr->sin_port = htons(localPort);
 
-    if ((n = getaddrinfo(host, service, &hints, &res)) != 0)
-        throw ProbeException("getaddrinfo error", gai_strerror(n));
-
-    bzero(&addr, sizeof(struct sockaddr));
-    memcpy(&addr, res->ai_addr, sizeof(struct sockaddr));
-    addrlen = res->ai_addrlen;
-
-    freeaddrinfo(res);
 }
 
